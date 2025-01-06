cb611d7b190ab3ae5c6b549bb6011124ed0b1b93
AbhishekMaurya575 <abhishek.maurya257185@gmail.com>
2025-01-06 19:02:25 +0530

    Create config.js

---
+++ src/appwrite/config.js
@@ -0,0 +1,61 @@
+import { Client, Databases, Query } from "appwrite";
+import conf from "../conf/conf";
+
+export class Service {
+  Client = new Client();
+  database;
+
+  constructor() {
+    this.Client.setEndpoint(conf.appwriteUrl).setProject(
+      conf.appwriteProjectId
+    );
+    this.database = new Databases(this.Client);
+  }
+
+  async createDocument(data, title) {
+    try {
+      const result = await this.database.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        title,
+        data
+      );
+      return result;
+    } catch (error) {
+      console.error("Error creating document:", error);
+      throw error;
+    }
+  }
+
+  async updateDocument(data, title) {
+    try {
+      const result = await this.database.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        title,
+        data
+      );
+      return result;
+    } catch (error) {
+      console.error("Error updating document:", error);
+      throw error;
+    }
+  }
+
+  async getDocument(username, password) {
+    try {
+      const result = await this.database.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        [Query.equal("username", username), Query.equal("password", password)]
+      );
+      return result.documents[0];
+    } catch (error) {
+      console.error("Error getting document:", error);
+      throw error;
+    }
+  }
+}
+
+const service = new Service();
+export default service;
