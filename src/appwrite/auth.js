b97e956b94d395291eabb3e9c1f90b48f5a24db0
AbhishekMaurya575 <abhishek.maurya257185@gmail.com>
2025-01-06 19:02:34 +0530

    Create auth.js

---
+++ src/appwrite/auth.js
@@ -0,0 +1,27 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+  Client = new Client();
+  account;
+
+  constructor() {
+    this.Client.setEndpoint(conf.appwriteUrl).setProject(
+      conf.appwriteProjectId
+    );
+    this.account = new Account(this.Client);
+  }
+
+  async createAccount(password) {
+    try {
+      const userAccount = await this.account.create(ID.unique(), password);
+      return userAccount;
+    } catch (error) {
+      console.error("Error creating account:", error);
+      throw error;
+    }
+  }
+}
+
+const authservice = new AuthService();
+export default authservice;
