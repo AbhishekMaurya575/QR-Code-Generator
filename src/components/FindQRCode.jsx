4c0e35db1a339c42774ce802b99fcc6b42f7585f
AbhishekMaurya575 <abhishek.maurya257185@gmail.com>
2025-01-06 19:02:16 +0530

    Update FindQRCode.jsx

--- src/components/FindQRCode.jsx
+++ src/components/FindQRCode.jsx
@@ -1,24 +1,59 @@
+import { useState } from "react";
 import React from "react";
+import service from "../appwrite/config";
 
 const FindQRCode = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [documentData, setDocumentData] = useState(null);
+  const [qrCodeUrl, setQrCodeUrl] = useState("");
+
+  const fetchDocument = async () => {
+    try {
+      const document = await service.getDocument(username, password);
+      if (document) {
+        setDocumentData(document);
+        setQrCodeUrl(document.data2);
+      } else {
+        alert("Invalid username or password. Please try again.");
+      }
+    } catch (err) {
+      console.error("Error fetching document:", err);
+      alert(
+        "Document with the requested username could not be found. Please check the username and try again."
+      );
+    }
+  };
+
   return (
     <div className="mx-auto max-w-lg bg-purple-100 my-16 p-8 rounded-lg flex flex-col gap-4 h-[67vh]">
       <h1 className="font-bold text-2xl">Find your QR Code</h1>
       <div className="flex flex-col gap-2">
         <input
           type="text"
-          className="p-4 py-2 rounded-md focus:outline-purple-600"
-          placeholder="Enter your short URL text"
+          id="username"
+          className="w-full text-xl pl-2 outline-none mt-4"
+          placeholder="Enter your username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           type="password"
-          className="p-4 py-2 rounded-md focus:outline-purple-600"
-          placeholder="Enter your password to get the Full link"
+          id="password"
+          className="w-full text-xl pl-2 outline-none mt-4"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-
-        <button className="font-bold text-white my-3 bg-purple-500 rounded-lg shadow-lg p-3 py-1">
-          Generate
+        <button
+          className="font-bold text-white my-3 bg-purple-500 rounded-lg shadow-lg p-3 py-1"
+          onClick={fetchDocument}
+        >
+          Fetch Document
         </button>
+      </div>
+      <div className="flex justify-center items-center bg-red-500 w-full h-full rounded-3xl">
+        {qrCodeUrl && <img src={qrCodeUrl} alt="QR Code" />}
       </div>
     </div>
   );
